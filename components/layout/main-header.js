9eefd1892fa438e6d5d2fb9134d5a144a985c86d
Fabio Mesquita <fabio.mesquita@mindera.com>
2023-07-12 14:33:39 +0100

    [FIX] NavBar fix

--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -1,19 +1,54 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import classes from "./main-header.module.css";
 
 function MainHeader() {
+  const router = useRouter();
+  const [activePath, setActivePath] = useState(router.pathname);
+
+  useEffect(() => {
+    setActivePath(router.pathname);
+  }, [router.pathname]);
+
+  console.log(activePath);
+
   return (
     <header className={classes.header}>
       <nav className={classes.navigation}>
         <div className={classes.navLinks}>
-          <Link href="/">Homepage</Link>
-          <Link href="/aboutMe">About me</Link>
-          <Link href="/portfolio">Portfolio</Link>
-          <Link href="/experience">Experience</Link>
-          <Link href="/contacts">Contacts</Link>
+          <CustomLink href="/" label="Homepage" isActive={activePath === "/"} />
+          <CustomLink
+            href="/aboutMe"
+            label="About me"
+            isActive={router.pathname === "/aboutMe"}
+          />
+          <CustomLink
+            href="/portfolio"
+            label="Portfolio"
+            isActive={router.pathname === "/portfolio"}
+          />
+          <CustomLink
+            href="/experience"
+            label="Experience"
+            isActive={router.pathname === "/experience"}
+          />
+          <CustomLink
+            href="/contacts"
+            label="Contacts"
+            isActive={router.pathname === "/contacts"}
+          />
         </div>
       </nav>
     </header>
+  );
+}
+
+function CustomLink({ href, label, isActive }) {
+  return (
+    <Link className={isActive ? classes.active : ""} href={href} passHref>
+      {label}
+    </Link>
   );
 }
 
